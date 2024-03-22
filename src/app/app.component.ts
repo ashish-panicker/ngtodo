88f103818a3a29edb7ecff3157cb01bf4b10f304
ashish-panicker <ashish.s.panicker@gmail.com>
2024-03-22 12:48:27 +0530

    Angular with firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodosFirebaseService } from './todos-firebase.service';
+import Todo from './todo';
+import { NgFor } from '@angular/common';
+import { LoremIpsum } from 'lorem-ipsum';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ngtodo';
+
+  todoService = inject(TodosFirebaseService)
+  todos: Todo[] = []
+  lorem = new LoremIpsum();
+
+  loadTodos() {
+    this.todoService.getTodos().subscribe(todos => this.todos = todos)
+  }
+
+  newTodo() {
+    this.todoService.addTodo(this.lorem.generateSentences(1)).subscribe(id => console.log(id))
+  }
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,27 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(firebaseConfig)),
+      provideFirestore(() => getFirestore())
+    ])
+  ]
 };
